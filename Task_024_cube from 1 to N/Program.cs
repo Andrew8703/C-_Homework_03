f3e82b6af9fab425333f406a5d8dd8fb219cd8d8
Andrew <and_0@bk.ru>
2022-02-13 21:03:42 +0400

    Сделал задачу: Найти кубы чисел от 1 до N

---
+++ Task_024_cube from 1 to N/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+Console.Write("Введите верхний предел диапазона: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Диапазон от 1 до " + N + "\t");
+for (int i = 1; i <= N; i++)
+    Console.Write(i + "\t");
+Console.WriteLine();
+for (int i = 1; i <= N; i++)
+    Console.Write(i * i * i + "\t");
