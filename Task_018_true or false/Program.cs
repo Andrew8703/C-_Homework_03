254721df9d97941a4d317c019a2eb5e7963cd193
Andrew <and_0@bk.ru>
2022-02-13 20:22:26 +0400

    Сделал задачу Найти расстояние между точками в пространстве 2D/3D

---
+++ Task_018_true or false/Program.cs
@@ -0,0 +1,9 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+bool x = true;
+bool y = true;
+if (!(x || y) == (!x && !y))
+{
+    Console.Write("True");
+}
+else
+Console.Write("False");
---
+++ Task_022_distance 2D3D/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+int x1 = new Random().Next(-10, 10); int y1 = new Random().Next(-10, 10); int z1 = new Random().Next(-10, 10);
+int x2 = new Random().Next(-10, 10); int y2 = new Random().Next(-10, 10); int z2 = new Random().Next(-10, 10);
+double distance = 0;
+Console.WriteLine("x1 = " + x1 + " y1 = " + y1 + " z1 = " + z1);
+Console.WriteLine("x2 = " + x2 + " y2 = " + y2 + " z2 = " + z2);
+if (z1 == 0 && z2 == 0)
+{
+    distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+    Console.WriteLine("Расстояние между точками в 2D пространстве = " + Math.Round(distance,3));
+}
+else
+{
+    distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+    Console.WriteLine("Расстояние между точками в 3D пространстве = " + Math.Round(distance,3));
+}
